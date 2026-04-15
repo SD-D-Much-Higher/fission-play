9e9d6cc48035e628ca10a0beaf8b94dabae5c4f1
whhlct <whhlct22@gmail.com>
2026-04-15 19:51:37 -0400

    feat: Seemingly working implementation of auth

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,10 @@
 import os
 
 from pymongo import AsyncMongoClient
-from beanie import init_beanie
+from beanie import init_beanie, Document
+from fastapi_users.db import BeanieBaseUser
+from fastapi_users_db_beanie import BeanieBaseUserDocument
+from fastapi_users_db_beanie import BeanieUserDatabase
 
 from dotenv import load_dotenv
 
@@ -13,10 +16,20 @@ from app.models.games import Game
 load_dotenv()
 
 
+class User(BeanieBaseUserDocument):
+    pass
+
+
+async def get_user_db():
+    yield BeanieUserDatabase(User)
+
+
 async def init_db():
     # Initialize the MongoDB client and Beanie ODM
     # Connect using env vars MONGO_URI and DB_NAME, with defaults if not set
     client = AsyncMongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017/"))
     db_name = os.getenv("DB_NAME", "appdb")
 
-    await init_beanie(database=client[db_name], document_models=[Player, Team, Game])
+    await init_beanie(
+        database=client[db_name], document_models=[Player, Team, Game, User]
+    )
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,37 @@ from fastapi import FastAPI
 
 from app.routes import players, teams, games
 from app.db.database import init_db
+from app.models.users import UserRead, UserCreate, UserUpdate
+from app.users import auth_backend, current_active_user, fastapi_users
 
 app = FastAPI(title="Fission Play API")
 app.include_router(players.router)
 app.include_router(teams.router)
 app.include_router(games.router)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
 
 
 @app.on_event("startup")
